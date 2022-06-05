39bfee4f61c05ea2ae9e67aedc697fb438fcfba8
Lucas Souza <lucasp.sdev@gmail.com>
2022-06-05 13:31:48 -0300

    feat: layer rule

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
   <hr />
   <ExtendRule />
   <hr />
+  <LayerRule />
 </template>
 
 <script lang="ts">
@@ -12,6 +13,7 @@ import { defineComponent } from "vue";
 import SubGrids from "./components/SubGrids/SubGrids.vue";
 import ContainerQueries from "./components/ContainerQueries/ContainerQueries.vue";
 import ExtendRule from "./components/ExtendRule/ExtendRule.vue";
+import LayerRule from "./components/LayerRule/LayerRule.vue";
 
 export default defineComponent({
   name: "App",
@@ -19,6 +21,7 @@ export default defineComponent({
     SubGrids,
     ContainerQueries,
     ExtendRule,
+    LayerRule,
   },
 });
 </script>
---
+++ src/components/LayerRule/LayerRule.vue
@@ -0,0 +1,48 @@
+<template>
+  <main>
+    <div class="layer-rule">
+      <div class="layer-rule__container">
+        <h1 class="layer-rule__title">
+          <span class="layer-rule__container-focus"> SHREK </span> Written by
+          William Steig & Ted Elliott
+        </h1>
+        <p class="layer-rule__container-paragraph">
+          Once upon a time there was a lovely princess. But she had an
+          enchantment upon her of a fearful sort which could only be broken by
+          love's first kiss. She was locked away in a castle guarded by a
+          terrible fire-breathing dragon. Many brave knights had attempted to
+          free her from this dreadful prison, but non prevailed. She waited in
+          the dragon's keep in the highest room of the tallest tower for her
+          true love and true love's first kiss. (laughs) Like that's ever gonna
+          happen. What a load of - (toilet flush)
+        </p>
+        <a href="#" class="layer-rule__container-button">Click me</a>
+      </div>
+    </div>
+    <div class="layer-rule__container">
+      <h2>Tombs pie Ettenmoors deserted single sacks private.</h2>
+      <p class="layer-rule__container-paragraph">
+        Yes, well, <a href="#">actually, that would be a </a> giant. Now, ogres,
+        oh they're much worse. They'll make a suit from your freshly peeled
+        skin.
+      </p>
+      <h3 class="layer-rule__subtitle">
+        Yeah, it'll grind your bones for it's bread.
+      </h3>
+      <p class="layer-rule__container-paragraph">
+        They'll shave your liver. Squeeze the jelly from your eyes! Actually,
+        it's quite good on toast.
+      </p>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "LayerRule",
+});
+</script>
+
+<style src="./LayerRule.scss" lang="scss" scoped />
